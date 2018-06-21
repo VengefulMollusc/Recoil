c97a350e10410f553df7f089423c6e3dbdd68b5a
Chris Read <ChrisR@intergen.org.nz>
2018-06-21 14:29:27 +1200

    e16 colour height blending shader

--- Assets/Landmass Generation/Data/TextureData.cs
+++ Assets/Landmass Generation/Data/TextureData.cs
@@ -2,11 +2,31 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[CreateAssetMenu]
+public class TextureData : UpdatableData
+{
+    public Color[] baseColours;
+    [Range(0, 1)]
+    public float[] baseStartHeights;
 
-public class TextureData : UpdatableData {
+    private float savedMinHeight;
+    private float savedMaxHeight;
 
     public void ApplyToMaterial(Material material)
     {
-        //
+        material.SetInt("baseColourCount", baseColours.Length);
+        material.SetColorArray("baseColours", baseColours);
+        material.SetFloatArray("baseStartHeights", baseStartHeights);
+
+        UpdateMeshHeights(material, savedMinHeight, savedMaxHeight);
+    }
+
+    public void UpdateMeshHeights(Material material, float minHeight, float maxHeight)
+    {
+        savedMinHeight = minHeight;
+        savedMaxHeight = maxHeight;
+
+        material.SetFloat("minHeight", minHeight);
+        material.SetFloat("maxHeight", maxHeight);
     }
 }
