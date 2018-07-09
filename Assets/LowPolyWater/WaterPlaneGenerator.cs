30faab53f5fdf095ab13dbcaf929717c24e004df
Chris Read <ChrisR@intergen.org.nz>
2018-07-09 15:37:01 +1200

    added v. basic plane generator with perlin noise

---
+++ Assets/LowPolyWater/WaterPlaneGenerator.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaterPlaneGenerator : MonoBehaviour
+{
+    public float size = 1f;
+    public int gridSize = 16;
+
+    private MeshFilter meshFilter;
+
+    // Use this for initialization
+    private void Start()
+    {
+        meshFilter = GetComponent<MeshFilter>();
+        meshFilter.mesh = GenerateMesh();
+    }
+
+    private Mesh GenerateMesh()
+    {
+        Mesh mesh = new Mesh();
+
+        List<Vector3> vertices = new List<Vector3>();
+        List<Vector3> normals = new List<Vector3>();
+        List<Vector2> uvs = new List<Vector2>();
+
+        for (int x = 0; x <= gridSize; x++)
+        {
+            for (int y = 0; y <= gridSize; y++)
+            {
+                vertices.Add(new Vector3(-size * 0.5f + size * (x / (float)gridSize), 0, -size * 0.5f + size * (y / (float)gridSize)));
+                normals.Add(Vector3.up);
+                uvs.Add(new Vector2(x / (float)gridSize, y / (float)gridSize));
+            }
+        }
+
+        List<int> triangles = new List<int>();
+        int vertCount = gridSize + 1;
+
+        for (int i = 0; i < vertCount * vertCount - vertCount; i++)
+        {
+            if ((i + 1) % vertCount == 0)
+            {
+                continue;
+            }
+            triangles.AddRange(new List<int>()
+            {
+                i + 1 + vertCount, i + vertCount, i,
+                i, i + 1, i + 1 + vertCount
+            });
+        }
+
+        mesh.SetVertices(vertices);
+        mesh.SetNormals(normals);
+        mesh.SetUVs(0, uvs);
+        mesh.SetTriangles(triangles, 0);
+
+        return mesh;
+    }
+}
