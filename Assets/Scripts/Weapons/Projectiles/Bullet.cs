bc186ac9835db91690c9cb51e009d2adc77610d2
Chris Read <ChrisR@intergen.org.nz>
2018-07-17 16:35:07 +1200

    basic machinegun

---
+++ Assets/Scripts/Weapons/Projectiles/Bullet.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Poolable))]
+public class Bullet : MonoBehaviour
+{
+    public float lifeSpan = 1f;
+    public float damage = 10f;
+    public float distancePerSecond = 100f;
+
+    private Poolable poolable;
+    private Vector3 movementVector;
+
+    private float timer;
+
+    public void Launch(Vector3 position, Vector3 direction)
+    {
+        transform.position = position;
+        movementVector = direction.normalized * distancePerSecond;
+        timer = 0f;
+        gameObject.SetActive(true);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!gameObject.activeSelf)
+            return;
+
+        // raycast ahead for collisions
+        RaycastHit hitInfo;
+        if (Physics.Raycast(transform.position, movementVector, out hitInfo, movementVector.magnitude))
+        {
+            float distance = hitInfo.distance;
+            transform.position += movementVector.normalized * distance * Time.deltaTime;
+            Collide(hitInfo.collider);
+            return;
+        }
+
+        transform.position += movementVector * Time.deltaTime;
+        timer += Time.deltaTime;
+
+        if (timer >= lifeSpan)
+        {
+            Despawn();
+        }
+    }
+
+    void Collide(Collider col)
+    {
+        HealthController healthController = col.gameObject.GetComponent<HealthController>();
+        if (healthController != null)
+        {
+            healthController.Damage(damage);
+        }
+        Despawn();
+    }
+
+    void Despawn()
+    {
+        GameObjectPoolController.Enqueue(GetComponent<Poolable>());
+    }
+}
---
+++ Assets/Scripts/Weapons/WeaponTypes/MachineGun.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MachineGun : Weapon
+{
+    public GameObject bulletPrefab;
+    public float fireRate = 0.2f;
+
+    private bool firing;
+    private bool firingSequenceActive;
+
+    private string poolableBulletKey;
+
+    void Start()
+    {
+        poolableBulletKey = bulletPrefab.GetComponent<Poolable>().key;
+        int poolableCount = (int)(bulletPrefab.GetComponent<Bullet>().lifeSpan / fireRate);
+        GameObjectPoolController.AddEntry(poolableBulletKey, bulletPrefab, poolableCount, poolableCount * 4);
+    }
+
+    public override void FireWeapon(bool pressed)
+    {
+        firing = pressed;
+        if (pressed && !firingSequenceActive)
+        {
+            StartCoroutine(FiringSequenceCoroutine());
+        }
+    }
+
+    /*
+     * Handles timing of weapon firing to not exceed firerate
+     */
+    private IEnumerator FiringSequenceCoroutine()
+    {
+        firingSequenceActive = true;
+
+        while (firing)
+        {
+            Fire();
+            yield return new WaitForSeconds(fireRate);
+        }
+
+        firingSequenceActive = false;
+    }
+
+    /*
+     * Get a bullet object from the pool controller
+     */
+    private void Fire()
+    {
+        Debug.Log("Fire MG");
+        Vector3 origin = transform.position;
+        Vector3 direction = transform.forward;
+
+        Bullet bullet = GameObjectPoolController.Dequeue(poolableBulletKey).GetComponent<Bullet>();
+        bullet.Launch(origin, direction);
+    }
+}
