cd2ad09d11cdd2956ae303ce4f95e6799ae7de92
VengefulMollusc <ennead_coil@hotmail.com>
2018-07-19 23:13:42 +1200

    scaled up terrain generation

--- Assets/Landmass Generation/Data/TerrainDataPackage.cs
+++ Assets/Landmass Generation/Data/TerrainDataPackage.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [CreateAssetMenu]
-public class TerrainDataPackage : ScriptableObject
+public class TerrainDataPackage : UpdatableData
 {
     public HeightMapSettings heightMapSettings;
     public MeshSettings meshSettings;
