a5fc6f269842e75bb997b55f0a03587899727350
Chris Read <ChrisR@intergen.org.nz>
2018-07-20 16:12:54 +1200

    split playerInputSettings into seperate data object

---
+++ Assets/Scripts/Data/PlayerInputSettings.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class PlayerInputSettings : ScriptableObject
+{
+    [Header("Axes")]
+    public string xMovAxis = "Horizontal";
+    public string yMovAxis = "Vertical";
+    public string xCamAxis = "LookX";
+    public string yCamAxis = "LookY";
+
+    public bool useWeaponAxes;
+    public string mainWeaponAxis = "WeaponMain";
+    public string secondaryWeaponAxis = "WeaponSecondary";
+
+    [Header("Buttons")]
+    public string boostButton = "Jump";
+
+    public string mainWeaponButton = "Fire1";
+    public string secondaryWeaponButton = "Fire2";
+}
--- Assets/Scripts/ScenePlayerController.cs
+++ Assets/Scripts/ScenePlayerController.cs
@@ -73,26 +73,6 @@ public class PlayerSettings
 }
 
 [System.Serializable]
-public class PlayerInputSettings
-{
-    [Header("Axes")]
-    public string xMovAxis = "Horizontal";
-    public string yMovAxis = "Vertical";
-    public string xCamAxis = "LookX";
-    public string yCamAxis = "LookY";
-
-    public bool useWeaponAxes;
-    public string mainWeaponAxis = "WeaponMain";
-    public string secondaryWeaponAxis = "WeaponSecondary";
-
-    [Header("Buttons")]
-    public string boostButton = "Jump";
-
-    public string mainWeaponButton = "Fire1";
-    public string secondaryWeaponButton = "Fire2";
-}
-
-[System.Serializable]
 public class ViewportSettings
 {
     public List<Rect> viewports;
