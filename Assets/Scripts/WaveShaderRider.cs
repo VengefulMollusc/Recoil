b2c99e7aec3284b437b6c6e4fdd4cbbdaa9ea8ac
Chris Read <ChrisR@intergen.org.nz>
2018-08-03 10:36:35 +1200

    added test script to track recalculated position and normal of wave

---
+++ Assets/Scripts/WaveShaderRider.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaveShaderRider : MonoBehaviour
+{
+
+    public WaveShaderPositionTracker wave;
+    public bool trackVerticalOnly;
+
+    void Update()
+    {
+        WavePositionInfo info = wave.CalculateDepthAndNormalAtPoint(transform.position);
+        if (trackVerticalOnly)
+            transform.position = new Vector3(transform.position.x, info.position.y, transform.position.z);
+        else
+            transform.position = info.position;
+        transform.rotation = Quaternion.LookRotation(info.normal);
+    }
+}
